ac19fbc0514668d9d7452af55d163e577ffedb68
IPKIN9 <irwandirini@gmail.com>
2021-05-02 04:25:59 +0800

    Installation maps is done

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'web\MasterController@index')->name('master.index');
